c83ac5c57b2ad69a2a7b254b04c88fc0ac889d2a
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-09-21 15:53:58 +0600

    updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,105 @@
 #include <TinyGPS++.h>
+#include <SoftwareSerial.h>
 
+// Choose two Arduino pins to use for software serial
+int RXPin = 2;
+int TXPin = 3;
+
+int GPSBaud = 9600;
+
+// Create a TinyGPS++ object
 TinyGPSPlus gps;
+
+// Create a software serial port called "gpsSerial"
+SoftwareSerial gpsSerial(RXPin, TXPin);
+
+void displayInfo(); // Forward declaration of displayInfo
 
 void setup()
 {
-  Serial.begin(115200);                    // For debugging
-  Serial1.begin(9600, SERIAL_8N1, 16, 17); // For GPS module (TX=16, RX=17)
+  // Start the Arduino hardware serial port at 9600 baud
+  Serial.begin(9600);
+
+  // Start the software serial port at the GPS's default baud
+  gpsSerial.begin(GPSBaud);
 }
 
 void loop()
 {
-  // Continuously read from the GPS module
-  while (Serial1.available() > 0)
+  // This sketch displays information every time a new sentence is correctly encoded.
+  while (gpsSerial.available() > 0)
+    if (gps.encode(gpsSerial.read()))
+    {
+      displayInfo();
+    }
+  if (millis() > 5000 && gps.charsProcessed() < 10)
   {
-    gps.encode(Serial1.read());
-
-    // Check if valid location data is available
-    if (gps.location.isValid())
-    {
-      Serial.println("GPS Data:");
-      Serial.print("Latitude: ");
-      Serial.println(gps.location.lat(), 6);
-      Serial.print("Longitude: ");
-      Serial.println(gps.location.lng(), 6);
-      Serial.print("Speed (km/h): ");
-      Serial.println(gps.speed.kmph());
-      Serial.print("Satellites: ");
-      Serial.println(gps.satellites.value());
-      Serial.print("Altitude (m): ");
-      Serial.println(gps.altitude.meters());
-    }
-    else
-    {
-      Serial.println("No valid GPS data available.");
-    }
+    Serial.println("No GPS detected");
+    while (true)
+      ;
   }
+}
+
+void displayInfo()
+{
+  if (gps.location.isValid())
+  {
+    Serial.print("Latitude: ");
+    Serial.println(gps.location.lat(), 6);
+    Serial.print("Longitude: ");
+    Serial.println(gps.location.lng(), 6);
+    Serial.print("Altitude: ");
+    Serial.println(gps.altitude.meters());
+    Serial.print("Speed in m/s = ");
+    Serial.println(gps.speed.mps());
+    Serial.print("Number of satellites in use = ");
+    Serial.println(gps.satellites.value());
+  }
+  else
+  {
+    Serial.println("Location: Not Available");
+  }
+
+  Serial.print("Date: ");
+  if (gps.date.isValid())
+  {
+
+    Serial.print(gps.date.day());
+    Serial.print("/");
+    Serial.print(gps.date.month());
+    Serial.print("/");
+    Serial.println(gps.date.year());
+  }
+  else
+  {
+    Serial.println("Not Available");
+  }
+
+  Serial.print("Time(UTC): ");
+  if (gps.time.isValid())
+  {
+    if (gps.time.hour() < 10)
+      Serial.print(F("0"));
+    Serial.print(gps.time.hour());
+    Serial.print(":");
+    if (gps.time.minute() < 10)
+      Serial.print(F("0"));
+    Serial.print(gps.time.minute());
+    Serial.print(":");
+    if (gps.time.second() < 10)
+      Serial.print(F("0"));
+    Serial.print(gps.time.second());
+    Serial.print(".");
+    if (gps.time.centisecond() < 10)
+      Serial.print(F("0"));
+    Serial.println(gps.time.centisecond());
+  }
+  else
+  {
+    Serial.println("Not Available");
+  }
+
+  Serial.println();
+  Serial.println();
+  delay(1000);
 }
